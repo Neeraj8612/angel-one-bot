75fe9da31152127fc222d67c6935efc4be47295b
Neeraj8612 <neerajsingh86267@gmail.com>
2025-09-26 13:43:14 +0530

    Update EMRcomplet.py

--- EMRcomplet.py
+++ EMRcomplet.py
@@ -302,9 +302,9 @@ class TradingBot:
                 self.active_trade = None
                 self.save_state()
             
-   def run_strategy_loop(self):
-       if not self.obj and not self.login(): self.running = False; return
-       if not self.instrument_list: self.status, self.running = "Error: Instrument list not loaded.", False; return
+     def run_strategy_loop(self):
+         if not self.obj and not self.login(): self.running = False; return
+         if not self.instrument_list: self.status, self.running = "Error: Instrument list not loaded.", False; return
     
     indices_to_monitor = self.params.get('indices_to_monitor', [])
     is_paper = self.params.get('is_paper_trading', False)
@@ -590,6 +590,7 @@ if st.button("▶ अभी बैकटेस्ट चलाएं"):
                 st.pyplot(fig)
 
                 
+
 
 
 
